b7945f0a4e17f6a78e5985f5186b7d3d464ec80d
Mustafa ip <16633480+Mustafa1p@users.noreply.github.com>
2024-07-12 02:04:47 -0700

    Update check_key.php

--- check_key.php
+++ check_key.php
@@ -1,10 +1,8 @@
 <?php
-$valid_keys = [
-  "123123",
-  "911"
-  
-  ];
+// محتويات ملف active_keys.txt
+$valid_keys = ["123123"];
 
+// الحصول على المفتاح من طلب POST
 $activation_code = isset($_POST['activation_code']) ? $_POST['activation_code'] : '';
 
 if (in_array($activation_code, $valid_keys)) {
